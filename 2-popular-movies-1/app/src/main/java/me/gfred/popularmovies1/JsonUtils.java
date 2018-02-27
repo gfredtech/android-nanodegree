a5e3ff12fc036ad08e1a34ddd2260bae17b3b754
Gfred <gfredtech@gmail.com>
2018-02-27 16:28:09 +0300

    Added NetworkUtils

--- 2-popular-movies-1/app/src/main/java/me/gfred/popularmovies1/JsonUtils.java
+++ 2-popular-movies-1/app/src/main/java/me/gfred/popularmovies1/JsonUtils.java
@@ -1,9 +1,40 @@
 package me.gfred.popularmovies1;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.gfred.popularmovies1.models.Movie;
+
 /**
  * Created by Gfred on 2/27/2018.
  */
 
 public class JsonUtils {
-    
+    final String API_KEY = " ";
+
+    public static Movie parseMovie(String json) throws JSONException {
+        JSONObject reader = new JSONObject(json);
+        String originalTitle = reader.getString("original_title");
+        String imgURL = reader.getString("poster_path");
+        String overview = reader.getString("overview");
+        Double voteAverage = reader.getDouble("vote_average");
+        String releaseDate = reader.getString("release_date");
+
+        return new Movie(originalTitle, imgURL, overview, voteAverage, releaseDate);
+    }
+
+    public static List<Movie> parseListMovies(String json) throws JSONException {
+        JSONObject reader = new JSONObject(json);
+        JSONArray moviesJson = reader.getJSONArray("results");
+        List<Movie> movieList = new ArrayList<>();
+        for(int i = 0; i < 100; i++) {
+            movieList.add(parseMovie(moviesJson.getString(i)));
+        }
+
+        return movieList;
+    }
 }
--- 2-popular-movies-1/app/src/main/java/me/gfred/popularmovies1/MainActivity.java
+++ 2-popular-movies-1/app/src/main/java/me/gfred/popularmovies1/MainActivity.java
@@ -2,12 +2,39 @@ package me.gfred.popularmovies1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import me.gfred.popularmovies1.models.Movie;
 
 public class MainActivity extends AppCompatActivity {
+    List<Movie> movies;
+
+    @BindView(R.id.movie_recyclerview)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            movies = JsonUtils.parseListMovies("some string");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        RecyclerAdapter adapter = new RecyclerAdapter(this, movies);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        recyclerView.setAdapter(adapter);
+
     }
+
 }
