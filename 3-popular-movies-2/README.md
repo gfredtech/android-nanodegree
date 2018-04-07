bec773b0b5aad69249651d2c19491b21f71f8b3b
Gfred <gfredtech@gmail.com>
2018-04-08 02:16:18 +0300

    finishing touches

--- 3-popular-movies-2/README.md
+++ 3-popular-movies-2/README.md
@@ -1,3 +1,3 @@
-#Popular Movies - Stage 1
+Popular Movies - Stage 1
 
 To set your API key for movie database, open NetworkUtils.java and set the variable "apiKey" to your API key.
--- 3-popular-movies-2/app/src/main/java/me/gfred/popularmovies2/utils/NetworkUtils.java
+++ 3-popular-movies-2/app/src/main/java/me/gfred/popularmovies2/utils/NetworkUtils.java
@@ -20,7 +20,7 @@ public class NetworkUtils {
     private final static String PARAM_REVIEWS = "reviews";
     private final static String PARAM_TRAILERS = "videos";
     private final static String PARAM_API ="api_key";
-    private final static String apiKey = "b16c34514b915ef016d679c43db8e6a6";
+    private final static String apiKey = "*REMOVED*";
 
     public static URL buildPopularMoviesQuery() {
 
