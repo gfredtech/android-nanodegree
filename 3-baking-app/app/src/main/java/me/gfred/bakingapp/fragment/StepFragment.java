02dca3ed3ab2b6e3a6a2b3519007d1cd89876e29
Gfred <gfredtech@gmail.com>
2018-04-29 02:41:12 +0300

    Previous/Next navigation on Steps fragment

--- 3-baking-app/app/src/main/java/me/gfred/bakingapp/fragment/StepFragment.java
+++ 3-baking-app/app/src/main/java/me/gfred/bakingapp/fragment/StepFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -30,13 +31,60 @@ public class StepFragment extends Fragment {
     @BindView(R.id.video_view)
     ImageView imageView;
 
+    @BindView(R.id.button_previous)
+    Button buttonPrevious;
+
+    @BindView(R.id.button_next)
+    Button buttonNext;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_step, container, false);
         ButterKnife.bind(this, rootView);
+
         description.setText(mSteps.get(index).getDescription());
+        if(index == 0) {
+            buttonPrevious.setClickable(false);
+            buttonPrevious.setEnabled(false);
+        } else if(index + 1 >= mSteps.size()) {
+            buttonNext.setClickable(false);
+            buttonNext.setEnabled(false);
+        }
+
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    description.setText(mSteps.get(++index).getDescription());
+
+                    if(index + 1 >= mSteps.size()) {
+                        buttonNext.setEnabled(false);
+                        buttonNext.setClickable(false);
+                    }
+
+                    if(!buttonPrevious.isEnabled()) {
+                        buttonPrevious.setEnabled(true);
+                        buttonPrevious.setClickable(true);
+                    }
+            }
+        });
+
+        buttonPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                description.setText(mSteps.get(--index).getDescription());
+                if(index - 1 < 0) {
+                    buttonPrevious.setEnabled(false);
+                    buttonPrevious.setClickable(false);
+                }
+                if(!buttonNext.isEnabled()) {
+                    buttonNext.setClickable(true);
+                    buttonNext.setEnabled(true);
+                }
+            }
+        });
         return rootView;
     }
 
