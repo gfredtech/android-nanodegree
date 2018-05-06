735fd07475026e459b9230812c3a21d8ad164dba
gfredtech <gfredtech@gmail.com>
2018-05-06 17:01:25 +0300

    Added sample test file and required gradle dependencies

---
+++ 3-baking-app/app/src/androidTest/java/me/gfred/bakingapp/RecipeActivityTest.java
@@ -0,0 +1,35 @@
+package me.gfred.bakingapp;
+
+
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import me.gfred.bakingapp.activity.RecipeActivity;
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeActivityTest {
+
+    @Rule
+    public ActivityTestRule<RecipeActivity> mTestRule =
+            new ActivityTestRule<>(RecipeActivity.class);
+
+    //TODO: Some reasonable tests here
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test public void test2() {
+
+    }
+
+    @Test public void test3() {
+
+    }
+
+}
