72d3b771f17539c8bb38d56f72c0e316928f9da0
gfredtech <gfredtech@gmail.com>
2018-05-06 14:02:36 +0300

    Removed unused import statements

--- 3-baking-app/app/src/main/java/me/gfred/bakingapp/activity/MainActivity.java
+++ 3-baking-app/app/src/main/java/me/gfred/bakingapp/activity/MainActivity.java
@@ -3,7 +3,6 @@ package me.gfred.bakingapp.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
--- 3-baking-app/app/src/main/java/me/gfred/bakingapp/fragment/StepFragment.java
+++ 3-baking-app/app/src/main/java/me/gfred/bakingapp/fragment/StepFragment.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -191,8 +190,8 @@ public class StepFragment extends Fragment {
         try {
             mCallback = (OnNavigationClickListener) context;
         } catch (ClassCastException e) {
-            e.printStackTrace();
-            Log.e("StepFragment", "Class must implement OnNavigationClickListener interface");
+            throw new ClassCastException(context.toString()
+                    + " must implement OnStepClickedListener");
         }
     }
 
