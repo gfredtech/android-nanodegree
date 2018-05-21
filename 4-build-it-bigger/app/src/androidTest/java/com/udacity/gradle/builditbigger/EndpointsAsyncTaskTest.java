eac4ea5e6fa1a4fc57b56ae8607db1f72aa13a34
gfredtech <gfredtech@gmail.com>
2018-05-21 12:12:52 +0300

    Added component test

---
+++ 4-build-it-bigger/app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -0,0 +1,32 @@
+package com.udacity.gradle.builditbigger;
+
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(AndroidJUnit4.class)
+public class EndpointsAsyncTaskTest {
+
+    @Test
+    public void asyncTaskDoesNotReturnEmptyString() throws
+            ExecutionException, InterruptedException {
+
+        EndpointsAsyncTask task = new EndpointsAsyncTask();
+
+        task.execute();
+
+        String joke = task.get();
+        assertNotNull(joke);
+        assertNotEquals("", joke);
+
+
+    }
+
+}
